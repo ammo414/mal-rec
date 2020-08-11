99f23fd1fbef4035672b8912fc812042db9bf8d2
ammo414 <58457535+ammo414@users.noreply.github.com>
2020-08-10 22:56:56 -0400

    Update main.py
    Write recommendations to file, error catching for username

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     site = 'https://myanimelist.net'
     if requests.head(site).status_code == 200:
 
-        initq = input('MAL or user input?')
+        initq = input('MAL or manual input? ')
         if re.match(r'(?i)MAL', initq):
             user  = input('username: ')
 
@@ -24,7 +24,17 @@ if __name__ == '__main__':
         recs = recpull(compcurr, idlst)
         final = finallist(recs, compcurr)
 
-        print(f'{user}\'s recommendations:\n')
+        try:
+            print(f'{user}\'s Recommendations:\n')
+            with open(f'{user}_recommendation.txt', 'w') as file:
+                for x in final:
+                    file.write(f'{x}\n\n')
+        except NameError:
+            print('Recommendations:\n')
+            with open('recommendation.txt', 'w') as file:
+                for x in final:
+                    file.write(str(f'{x}\n\n'))
+
         for x in final:
             print(x, '\n')
 
